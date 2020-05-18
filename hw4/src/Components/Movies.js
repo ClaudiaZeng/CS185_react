2c23fd269cf03370d82faa9cbc2e4ab79141b5b0
Yuqing Zeng <claudia@Yuqings-MacBook-Pro.local>
2020-05-17 23:42:02 -0700

    change to https

--- hw4/src/Components/Movies.js
+++ hw4/src/Components/Movies.js
@@ -8,42 +8,42 @@ export class Movies extends Component {
         const moviesURL = [
             {
                 id: 1,
-                url: "http://www.omdbapi.com/?i=tt4154796&apikey=d908aef1",
+                url: "https://www.omdbapi.com/?i=tt4154796&apikey=d908aef1",
                 isopen: false,
             },
             {
                 id: 2,
-                url: "http://www.omdbapi.com/?i=tt3501632&apikey=d908aef1",
+                url: "https://www.omdbapi.com/?i=tt3501632&apikey=d908aef1",
                 isopen: false,
             },
             {
                 id: 3,
-                url: "http://www.omdbapi.com/?i=tt0458339&apikey=d908aef1",
+                url: "https://www.omdbapi.com/?i=tt0458339&apikey=d908aef1",
                 isopen: false,
             },
             {
                 id: 4,
-                url: "http://www.omdbapi.com/?i=tt4154756&apikey=d908aef1",
+                url: "https://www.omdbapi.com/?i=tt4154756&apikey=d908aef1",
                 isopen: false,
             },
             {
                 id: 5,
-                url: "http://www.omdbapi.com/?i=tt3480822&apikey=d908aef1",
+                url: "https://www.omdbapi.com/?i=tt3480822&apikey=d908aef1",
                 isopen: false,
             },
             {
                 id: 6,
-                url: "http://www.omdbapi.com/?i=tt2015381&apikey=d908aef1",
+                url: "https://www.omdbapi.com/?i=tt2015381&apikey=d908aef1",
                 isopen: false,
             },
             {
                 id: 7,
-                url: "http://www.omdbapi.com/?i=tt3896198&apikey=d908aef1",
+                url: "https://www.omdbapi.com/?i=tt3896198&apikey=d908aef1",
                 isopen: false,
             },
             {
                 id: 8,
-                url: "http://www.omdbapi.com/?i=tt6320628&apikey=d908aef1",
+                url: "https://www.omdbapi.com/?i=tt6320628&apikey=d908aef1",
                 isopen: false,
             },
         ]
